bf4e8b7a7e160e82a382ff7511b941097fa3dfaa
sunil singh <sunilp896@gmail.com>
2018-11-10 22:48:43 +0530

    test file

---
+++ src/test1.java
@@ -0,0 +1,20 @@
+
+public class test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		a = a + 10;
+		a = a * 10;
+		a = a + 10;
+		if(a<200){
+			a = a+3;
+			a = a*0;
+		}
+		a = a + a; 
+	}
+	
+}
---
+++ src/test2.java
@@ -0,0 +1,11 @@
+
+public class test2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		while (x < 100) {
+			x = x * 0;
+			x = x + 9;
+		}
+	}
+}
