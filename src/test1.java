85fb5f21e280659a287497084d8e9f353e5ea066
sunil singh <sunilp896@gmail.com>
2018-11-13 22:46:05 +0530

    change in test 1

--- src/test1.java
+++ src/test1.java
@@ -1,3 +1,5 @@
+import java_cup.internal_error;
+
 
 public class test1 {
 
@@ -7,14 +9,17 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=10;
+		int d =0;
 		a = a + 10;
-		a = a * 10;
-		a = a + 10;
+		int x = a * 10;
+		int y = 0;
+		int b = y + 10;
 		if(a<200){
 			a = a+3;
-			a = a*0;
+			d = x*0;
 		}
-		a = a + a; 
+		y = a + d + b; 
+		
 	}
 	
 }
