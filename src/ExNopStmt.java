c796386c712423d22a1ab3bbfa7d2b69a8d4f8b6
sunil singh <sunilp896@gmail.com>
2018-11-30 01:39:42 +0530

    constructor added

--- src/ExNopStmt.java
+++ src/ExNopStmt.java
@@ -2,7 +2,12 @@ import soot.jimple.internal.JNopStmt;
 
 
 public class ExNopStmt extends JNopStmt{
+	String nameStr;
+	
+	public ExNopStmt(String str) {
+		this.nameStr = str;
+	}
 	public String toString() {
-		return "NOP.E";
+		return "NOP."+this.nameStr;
 	}
 }
