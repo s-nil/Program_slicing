62606202d8266c7375f487e8c59ed846a609e851
sunil singh <sunilp896@gmail.com>
2018-11-30 01:38:33 +0530

    invoke expr handled

--- src/DDG.java
+++ src/DDG.java
@@ -8,6 +8,8 @@ import soot.Value;
 import soot.ValueBox;
 import soot.jimple.AssignStmt;
 import soot.jimple.Stmt;
+import soot.jimple.internal.JimpleLocal;
+import soot.jimple.internal.JimpleLocalBox;
 import soot.toolkits.graph.UnitGraph;
 import soot.toolkits.scalar.SimpleLocalDefs;
 
@@ -23,6 +25,7 @@ public class DDG {
 		
 		Body body = graph.getBody();
 		
+		//Analysis that provides an implementation of the LocalDefs interface.
 		SimpleLocalDefs localDefs = new SimpleLocalDefs(graph);
 		
 		du_man = new ManageDefUse(localDefs);
@@ -32,16 +35,71 @@ public class DDG {
 	}
 
 	private void process_units(Iterator unitsItr) {
-		while (unitsItr.hasNext()) {
-			Stmt unit = (Stmt) unitsItr.next();
-			
-			unit_list.add(unit);
-			
-			if (unit.containsInvokeExpr()) {
+		try {
+			while (unitsItr.hasNext()) {
+				Stmt unit = (Stmt) unitsItr.next();
+				unit_list.add(unit);
 				
+				if (unit.containsInvokeExpr()) {
+					//throw new RuntimeException("Method contains calls to other functions.");
+					try {
+						handle_invoke_expr(unit);
+					} catch (Exception e) {
+						System.err.println("contains function call.");
+					}
+					
+					//System.out.println(unit.getInvokeExpr());
+				}
+				if (unit instanceof AssignStmt) {
+					process_assign_stmt(unit);
+				}
 			}
-			if (unit instanceof AssignStmt) {
-				process_assign_stmt(unit);
+			
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	private void handle_invoke_expr(Stmt unit) {
+		Iterator jlIt = unit.getInvokeExpr().getUseBoxes().iterator();
+		
+		while (jlIt.hasNext()) {
+			ValueBox val = (ValueBox) jlIt.next();
+			//System.out.println(val.getValue());
+			if(val instanceof JimpleLocalBox){
+				List defList = du_man.get_def_list(val.getValue(), unit);
+				//System.out.println(defList);
+				
+				if (defList.size() == 0) {
+					continue;
+				}
+				
+				Iterator it = defList.iterator();
+				while (it.hasNext()) {
+					Unit def_unit = (Unit) it.next();
+					add_dep(unit, def_unit, StoreGraph.DATA);
+					Util.v().log("dataEdge", "DATA\t" + unit + "--->"+def_unit);
+				}
+			}
+		}
+		
+		
+		
+		Iterator argIt = unit.getInvokeExpr().getArgs().iterator();
+		while (argIt.hasNext()) {
+			Value val = (Value) argIt.next();
+			//System.out.println(val);
+			List def_List = du_man.get_def_list(val, unit);
+			
+			if(def_List.size() == 0){
+				continue;
+			}
+			
+			Iterator it = def_List.iterator();
+			while (it.hasNext()) {
+				Unit def_unit = (Unit) it.next();
+				add_dep(unit, def_unit, StoreGraph.DATA);
+				Util.v().log("dataEdge", "DATA\t" + unit + "--->"+def_unit);
 			}
 		}
 	}
@@ -52,8 +110,12 @@ public class DDG {
 		
 		while (useboxIt.hasNext()) {
 			Value use_val = ((ValueBox) useboxIt.next()).getValue();
-			
+			//System.out.println(use_val);
 			List def_List = du_man.get_def_list(use_val, unit);
+			
+			if(def_List.size() == 0){
+				continue;
+			}
 			
 			Iterator it = def_List.iterator();
 			
@@ -63,7 +125,7 @@ public class DDG {
 				//add dependency
 				add_dep(unit,def_unit,StoreGraph.DATA);
 				
-				Util.v().log("dataedge", "DATA\t" + def_unit + "\t" + unit + " ");
+				Util.v().log("dataEdge", "DATA\t" + unit + "---->" + def_unit);
 			}
 		}
 	}
